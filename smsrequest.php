b9ab0ac27f7e4f9fe8ca3cd8ea4c9bf117212158
Adrian <adrian@adrianblog.net>
2012-07-28 17:14:07 -0700

    Removed most of the stateful stuff and I'm trying to get just the basics to work. Code is basically mid-debug at the moment.

--- smsrequest.php
+++ smsrequest.php
@@ -1,31 +1,21 @@
 <?php
 require ('./query.php');
-// start the session
-session_start();
+// Allows me do debug!
+error_reporting(E_ALL);
+ini_set("display_errors",1);
 
-// This is the state we're in
-$state = $_SESSION['state'];
-// Previous query (if there is one)
-$state = $_SESSION['query'];
-
-// Create the state if you're ready!
-if(!strlen($state)) {
-	$state = 'QUERY';
-}
-
-$sender = $_POST["From"] // The phone number that sent this message.
-$query = $_POST["Body"] // The text body of the SMS message. Up to 160 characters long.
-$pieces = explode(",", $_POST["Body"], 2); // Split the query into two
-
+//setcookie("TestCookie", 'lolhi', time()+250); // Expires after 3-ish minutes 
+$message='Something went wrong.'; // The final message to be sent
+$query = $_POST["Body"]; // The text body of the SMS message. Up to 160 characters long.
+$pieces = explode(',', $query, 2); // Split the query into two
+// echo "lol uh oh";
 $results = get_results($pieces[0], $pieces[1]); // Pass in the data
 
-$message=''; // The final message to be sent
-
-if(count($results)>1) // Handles if there is more than result
+if(count($results)>-1) // Handles if there is more than one result (disabled for now)
 {
 	if($state = 'MORE' && strcasecmp( 'MORE' , $query )==0) // Set back to QUERY after a MORE
 	{
-		$message=$results[0]." ".$results[1];
+		$message=$results[0].' '.$results[1];
 		$state = 'QUERY';
 		$_SESSION['query']='';
 	}
@@ -36,7 +26,8 @@ if(count($results)>1) // Handles if there is more than result
 		$state = 'MORE';
 	}
 }
-if(count($results==1))
+
+if(count($results>=1))
 {
 	$message=$results[0]." ".$results[1];
 }
@@ -46,11 +37,9 @@ if(empty($result))
 	$message='No results found!';
 }
 
-$_SESSION['state'] = $state;
-
 header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-	<sms><?php echo $message; if(count($results)>1 && $state=='QUERY' ) ' Text MORE for more results.'; ?></sms>
+	<sms><?php echo $message;?><<?php if(count($results)>1) echo ' Text MORE for more results.'; ?>/sms>
 </Response>
